12e583d036452e898f10609b56ec147942eaabee
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2025-04-13 22:11:15 +0200

    refactor models and order creation; add related names and improve type hints

--- db/models.py
+++ db/models.py
@@ -69,7 +69,8 @@ class MovieSession(models.Model):
 class Order(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+                             on_delete=models.CASCADE,
+                             related_name="orders")
 
     class Meta:
         ordering = ["-created_at"]
@@ -82,8 +83,12 @@ class Order(models.Model):
 
 
 class Ticket(models.Model):
-    movie_session = models.ForeignKey("MovieSession", on_delete=models.CASCADE)
-    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    movie_session = models.ForeignKey("MovieSession",
+                                      on_delete=models.CASCADE,
+                                      related_name="tickets")
+    order = models.ForeignKey("Order",
+                              on_delete=models.CASCADE,
+                              related_name="tickets")
     row = models.IntegerField()
     seat = models.IntegerField()
 
--- services/order.py
+++ services/order.py
@@ -8,28 +8,26 @@ from db.models import Order, Ticket
 from services.movie_session import get_movie_session_by_id
 
 
+@transaction.atomic
 def create_order(tickets: list[dict], username: str, date: str = None) -> None:
-    with transaction.atomic():
-        new_order = Order()
-        new_order.user = get_user_model().objects.get(username=username)
+    new_order = Order()
+    new_order.user = get_user_model().objects.get(username=username)
 
-        if date:
-            new_order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+    if date:
+        new_order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
 
-        new_order.save()
+    new_order.save()
 
-        for ticket in tickets:
-            Ticket.objects.create(
-                order=new_order,
-                row=ticket.get("row"),
-                seat=ticket.get("seat"),
-                movie_session=get_movie_session_by_id(
-                    ticket.get("movie_session")
-                )
-            )
+    for ticket in tickets:
+        Ticket.objects.create(
+            order=new_order,
+            row=ticket.get("row"),
+            seat=ticket.get("seat"),
+            movie_session=get_movie_session_by_id(ticket.get("movie_session"))
+        )
 
 
-def get_orders(username: str = None) -> QuerySet:
+def get_orders(username: str = None) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
